9cb388f2d2cc75fd9c5fc376c1b30a3733a54260
Allen-Work <yxazy@foxmail.com>
2019-12-31 16:20:13 +0800

    删除项目

--- src/utils/GeneralUtils.js
+++
@@ -1,8 +0,0 @@
-//去掉最后一个逗号
-let removeTheLastComma = function removeTheLastComma(data) {
-  var index = data.lastIndexOf(",");
-  return data.substring(0,index);
-}
-export default {
-  removeTheLastComma
-}
